6f365fbcb7f82ff6cac801a1428c29b2e4b0f19d
QNeee <vovasagan7@gmail.com>
2023-03-07 18:06:49 +0300

    end

--- src/components/OnPendings/OnPendings.jsx
+++ src/components/OnPendings/OnPendings.jsx
@@ -1,10 +1,10 @@
 import { useSelector, useDispatch } from "react-redux";
 import { getFilter, getLoading, getUserFriends, getUserId, setFilterValue } from "Redux/networkSlice";
-import { Link } from "react-router-dom";
-import { FriendsContainer, FindDiv, FriendsDiv } from "../UserFriends/UserFriends.styled";
+import { FriendsContainer, FindDiv, FriendsDiv, NickItem } from "../UserFriends/UserFriends.styled";
 import { Button } from "components/App.styled";
 import { removeFriend, verifyFriend } from "Redux/friendsOperations";
 import noAvatar from '../../images/NoAvatar.webp';
+import { P } from "./OnPendings.styled";
 export const OnPendings = () => {
     const dispatch = useDispatch();
     const userFriends = useSelector(getUserFriends);
@@ -21,7 +21,7 @@ export const OnPendings = () => {
     }
     return <><FriendsContainer><FindDiv>Find Friend
         <input value={filter} onChange={(e) => dispatch(setFilterValue(e.target.value))} />
-    </FindDiv>{!loading && filteredFriends.length > 0 ? filteredFriends.map(item => <FriendsDiv key={item._id}><p><Link to={`/home/profile/${item.find}`}>{item.nickName}</Link></p><p><img src={item.avatarURL ? item.avatarURL : noAvatar} alt={item.nickName} width='250' height='250' /></p>
+    </FindDiv>{!loading && filteredFriends.length > 0 ? filteredFriends.map(item => <FriendsDiv key={item._id}><P><NickItem to={`/home/profile/${item.find}`}>{item.nickName}</NickItem></P><p><img src={item.avatarURL ? item.avatarURL : noAvatar} alt={item.nickName} width='250' height='250' /></p>
         {!loading && <Button onClick={() => onClickAccept(item.verificationToken)} type="button">Accept</Button>}
         <Button onClick={() => onClickDecline(item.find)} type="button">Decline</Button></FriendsDiv>) : <div>No friends</div>}
     </FriendsContainer></>
---
+++ src/components/OnPendings/OnPendings.styled.jsx
@@ -0,0 +1,9 @@
+
+
+import styled from "@emotion/styled";
+
+export const P = styled.p`
+display: flex;
+justify-content: center;
+align-items: center;
+`;
